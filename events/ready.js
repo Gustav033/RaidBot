8d4028709340c8fa06d8b595549388211b8677d9
Gustav033 <55931689+Gustav033@users.noreply.github.com>
2019-11-22 20:52:13 -0300

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,5 @@
+module.exports = async client => {
+        console.log("                 Raid-Bot V4 Online");
+        console.log("        Feito Por: Gustavo33");
+       client.user.setGame("Feito Por Gustavo33")
+}
